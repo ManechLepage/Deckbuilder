868eefba2e60b1cdcbeab223ea0c18e3b87f0dd4
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-08-28 15:54:00 -0400

    3: Flood fill algorithm for movement

--- scripts/Movement/Movement.gd
+++ scripts/Movement/Movement.gd
@@ -1,5 +1,28 @@
 class_name Movement
 extends Resource
 
-@export var relative_positions: Array[Vector2i]
+var relative_positions: Array[Vector2i]
 @export var relative_directions: Array[Vector2i]
+@export var distance: int
+
+var relative_neighbours = [
+	Vector2i(0, 1),
+	Vector2i(0, -1),
+	Vector2i(1, 0),
+	Vector2i(-1, 0)
+]
+
+var visited_nodes: Array[Vector2i]
+var grid: Array[Array]
+
+func calculate_relative_positions(_grid: Array[Array], position: Vector2i):
+	visited_nodes.clear()
+	grid = _grid
+	find_range(position, distance, 0)
+
+func find_range(position: Vector2i, max_range:int, current_range:int):
+	visited_nodes.append(position)
+	if current_range < max_range:
+		for i in relative_neighbours:
+			if grid[i.x][i.y] == 0:
+				find_range(position + i, max_range, current_range + 1)
